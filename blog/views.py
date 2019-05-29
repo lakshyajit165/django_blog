03b33318bb376b7ddf7da72e97cbed8701504499
Lakshyajit <lakshyajit@localhost.localdomain>
2019-05-29 23:35:39 +0530

    Update user profile and crud cont.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 
 # Create your views here.
@@ -9,6 +11,16 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+     model = Post
+     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+     context_object_name = 'posts'
+     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+     model = Post
+     
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
