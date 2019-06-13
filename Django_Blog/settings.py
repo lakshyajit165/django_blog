1c454059533d73765917e1ccc99fe648c63c67df
Lakshyajit <lakshyajit@localhost.localdomain>
2019-06-13 18:47:40 +0530

    Using postgresql

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -161,3 +162,4 @@ AWS_S3_REGION_NAME = "ap-south-1"
 
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 
+django_heroku.settings(locals())
